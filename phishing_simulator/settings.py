81895fb4b2f4800d957368451721099cf8e13527
Ciya Lalu <ciyalalu.7279@gmail.com>
2026-01-11 09:24:52 +0530

    Prepare project for Render deployment

--- phishing_simulator/settings.py
+++ phishing_simulator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gu7mx%dvr#$#y4e!f^pf8epr&(xy$g0!rmg=&sx%4z1y(@$__2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
