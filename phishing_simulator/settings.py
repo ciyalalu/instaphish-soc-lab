c0041260b0ce5b4efe12a4f2a70354934dd1f9e7
Ciya Lalu <ciyalalu.7279@gmail.com>
2026-01-11 10:10:53 +0530

    Fix DEBUG boolean for Render

--- phishing_simulator/settings.py
+++ phishing_simulator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gu7mx%dvr#$#y4e!f^pf8epr&(xy$g0!rmg=&sx%4z1y(@$__2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
